0a5328dcec7403b345c5ff172daf184dbb442729
Austin Paley <austinpaley32@gmail.com>
2018-02-28 21:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
- const rank = document.querySelectorAll('ul.rank-list .li')
+ const rank = document.querySelectorAll('ul.rank-list li')
  
  for (let i = 0; i < rank.length, i++;){
    rank[i].innerHTML = parseInt(rank[i].innerHTML + n)
