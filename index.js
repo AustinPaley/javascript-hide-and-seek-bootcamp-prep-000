44700a092d646f438f6a630355a0573dfa431946
Austin Paley <austinpaley32@gmail.com>
2018-02-28 22:02:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(`${selector}`)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+ const rank = document.querySelectorAll('ul.ranked-list li')
+ 
+ for (let i = 0; i < rank.length; i++){
+   rank[i].innerHTML = parseInt(rank[i].innerHTML) + n
+ }
+}
+
+function deepestChild(){
+  var node = document.querySelector('#grand-node').querySelectorAll('div')
+return node[node.length-1];
+}
