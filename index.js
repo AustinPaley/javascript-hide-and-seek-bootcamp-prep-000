b7beae5c1380fc5d7acc87f8f5a59baf12397e56
Austin Paley <austinpaley32@gmail.com>
2018-02-28 21:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n){
  const rank = document.querySelectorAll('.rank-list')
  
  for (let i = 0; i < rank.length i++;){
-   rank[i].innerHTML = parseInt
+   rank[i].innerHTML = parseInt(+n)
  }
 }
